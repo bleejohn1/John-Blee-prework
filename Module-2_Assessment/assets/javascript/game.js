71b17e716376ac2dd75c207c9fca7f9541b50495
John Blee <bleejohn1@github.com>
2020-06-14 10:30:36 -0500

    display; initialize function

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,9 +1,9 @@
-//function initializeValues(){
+
     const maximumTries = 10;
     lettersUserHasGuessed = [];
     wordBeingGuessed = [];
     var numGuessesLeft = maximumTries;
-    var numberOfWins = 3;
+     numberOfWins = 3;
     var currentLetter = "";
     wordsToGuessFrom = ["Washington",
     "Adams",
@@ -23,8 +23,30 @@ function showWins(numberOfWins){
     return numberOfWins;
 }
 
+
 const winsIH1 = document.querySelector('#winsI');
 const currentWordIH1 = document.querySelector('#currentWordI');
 const guessesLeftIH1 = document.querySelector('#guessesLeftI');
 const lettersGuessedIH1 = document.querySelector('#lettersGuessedI');
+
+//winsIH1.innerHTML = ('Hello');
+
+
+
+
+function practiceF(){
+    console.log(2);
+}
+
+document.addEventListener('keyup', showWins);
+
+function initializeValues()
+{
+    console.log(2);
+}
+
+
+
+
+
 
