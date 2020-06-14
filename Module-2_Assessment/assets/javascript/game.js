f26b74c3a0ff7b0279ba20abf4c25f7a9015bc0a
John Blee <bleejohn1@github.com>
2020-06-14 07:33:46 -0500

    update 1

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,13 @@
+function initialize(){
+    const maximumTries = 10;
+    lettersUserHasGuessed = [];
+    wordBeingGuessed = [];
+    var numGuessesLeft = maximumTries;
+    var numberOfWins = 0;
+    var currentLetter = "";
+}
+
+function getLetter(){
+
+    
+}
