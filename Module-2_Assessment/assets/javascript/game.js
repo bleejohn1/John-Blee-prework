5ad803101e301de91956ae29006bacfd660dd620
John Blee <bleejohn1@github.com>
2020-06-14 09:48:37 -0500

    update 1

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,13 +1,26 @@
-function initialize(){
+//function initializeValues(){
     const maximumTries = 10;
     lettersUserHasGuessed = [];
     wordBeingGuessed = [];
     var numGuessesLeft = maximumTries;
-    var numberOfWins = 0;
+    var numberOfWins = 3;
     var currentLetter = "";
+    wordsToGuessFrom = ["Washington",
+    "Adams",
+    "Jefferson",
+    "Madison",
+    "Monroe",
+    "Jackson",
+    "Tyler",
+    "Polk",
+    "Taylor",
+    "Fillmore",
+    "Pierce"]
+//}
+
+function showWins(numberOfWins){
+    console.log(numberOfWins);
+    return numberOfWins;
 }
 
-function getLetter(){
-
-    
-}
+const winsH1 = document.querySelector('#winsI');
